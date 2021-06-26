c0d19b5a6a49197b8897ea70cd6e384d846d45d7
pz-max <max.parzen@ed.ac.uk>
2021-06-26 15:29:33 +0100

    add polymask deer example

--- assets/js/custom_particle.js
+++ assets/js/custom_particle.js
@@ -96,7 +96,7 @@ tsParticles.load("tsparticles", {
         radius: 10
       },
       inlineArrangement: "equidistant",
-      scale: 0.5,
+      scale: 0.2,
       type: "inline",
       url: "https://particles.js.org/images/smalldeer.svg"
     }
