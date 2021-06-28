1a47115bcdab18c162167476e137ec971065bb7e
pz-max <max.parzen@ed.ac.uk>
2021-06-28 21:53:01 +0100

    new custom_particle

--- assets/js/custom_particle.js
+++ assets/js/custom_particle.js
@@ -1,12 +1,25 @@
 tsParticles.load("tsparticles", {
     detectRetina: false,
-    fpsLimit: 60,
     interactivity: {
         detectsOn: "canvas",
         events: {
+            onClick: {
+                enable: false,
+                mode: "bubble"
+            },
+            onDiv: {
+                elementId: "repulse-div",
+                enable: false,
+                mode: "repulse"
+            },
             onHover: {
                 enable: true,
-                mode: "bubble"
+                mode: "grab",
+                parallax: {
+                    enable: false,
+                    force: 2,
+                    smooth: 10
+                }
             },
             resize: true
         },
@@ -15,28 +28,51 @@ tsParticles.load("tsparticles", {
                 distance: 40,
                 duration: 2,
                 opacity: 8,
-                size: 6,
+                size: 10,
                 speed: 3
+            },
+            connect: {
+                distance: 80,
+                lineLinked: {
+                    opacity: 0.5
+                },
+                radius: 60
+            },
+            grab: {
+                distance: 300,
+                lineLinked: {
+                    opacity: 1
+                }
+            },
+            push: {
+                quantity: 4
+            },
+            remove: {
+                quantity: 2
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            },
+            slow: {
+                active: false,
+                radius: 0,
+                factor: 1
             }
         }
     },
     particles: {
         color: {
-            value: "#ff0000",
-            animation: {
-                enable: true,
-                speed: 20,
-                sync: true
-            }
+            value: ["#4285f4", "#34A853", "#FBBC05", "#EA4335"]
         },
         lineLinked: {
             blink: false,
             color: "random",
             consent: false,
-            distance: 30,
+            distance: 40,
             enable: true,
-            opacity: 0.3,
-            width: 0.5
+            opacity: 0.8,
+            width: 1
         },
         move: {
             attract: {
@@ -50,8 +86,8 @@ tsParticles.load("tsparticles", {
             direction: "none",
             enable: true,
             outMode: "bounce",
-            random: true,
-            speed: 0.5,
+            random: false,
+            speed: 1,
             straight: false
         },
         number: {
@@ -65,41 +101,47 @@ tsParticles.load("tsparticles", {
         opacity: {
             animation: {
                 enable: true,
-                minimumValue: 0.05,
+                minimumValue: 0.3,
                 speed: 2,
                 sync: false
             },
             random: false,
-            value: 1
+            value: 0.8
         },
         shape: {
+            character: {
+                fill: false,
+                font: "Verdana",
+                style: "",
+                value: "*",
+                weight: "400"
+            },
+            image: {
+                height: 500,
+                replaceColor: true,
+                src: "https://particles.js.org/images/github.svg",
+                width: 500
+            },
+            polygon: {
+                sides: 5
+            },
+            stroke: {
+                color: "#000000",
+                width: 0
+            },
             type: "circle"
         },
         size: {
             animation: {
                 enable: false,
                 minimumValue: 0.1,
-                speed: 40,
+                speed: 20,
                 sync: false
             },
             random: true,
-            value: 1
+            value: 5
         }
     },
-    // polygon: {
-    //     draw: {
-    //         enable: true,
-    //         lineColor: "rgba(255,255,255,0.2)",
-    //         lineWidth: 0.3
-    //     },
-    //     move: {
-    //         radius: 10
-    //     },
-    //     inlineArrangement: "equidistant",
-    //     scale: 1,
-    //     type: "inside",
-    //     url: "assets/img/smalldeer_africa.svg"
-    // },
     responsive: [{
             maxWidth: 600,
             options: {
